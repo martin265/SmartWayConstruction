30b01356884d3ead6cafc80ad319c1fa1e80b3d3
martin265 <martinsilungwe12@gmail.com>
2024-01-26 11:23:29 +0200

    still in progress

---
+++ applicant_apply_job.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--  ================= // including the top navigation bar here ========= -->
+    <div class="top-navigation-bar">
+        <?php include("templates/header.php"); ?>
+    </div>
+</body>
+</html>
