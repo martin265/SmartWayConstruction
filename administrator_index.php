da21f75303d075f9ddc5ae08b89aea6d89ae2a24
martin265 <martinsilungwe12@gmail.com>
2024-02-12 14:07:18 +0200

    still in progress

--- administrator_index.php
+++ administrator_index.php
@@ -17,7 +17,32 @@
         </div>
         
         <div class="content-area">
-            <!-- the php pages will reside in here -->
+            <!-- ============== the content for the administrator dashboard will be here ======= -->
+            <div class="container-xxl">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="main-dashboard-panel">
+                            <div class="top-welcome-page">
+                                <!-- =============== the top icon section will be here ========== -->
+                                <div class="top-icon">
+                                    <div class="top-welcome-text">
+                                        <h1><i class="bi bi-grid-1x2"></i> main dashboard</h1>
+                                    </div>
+                                </div>
+                                <!-- ================= the welcome section will be here ========= -->
+                            </div>
+
+                            <!-- ============== the section for the cards will be here ============ -->
+                            <div class="dashboard-cards">
+                                <div class="total-applicants">1</div>
+                                <div class="total-questions">2</div>
+                                <div class="total-jobs-available">3</div>
+                            </div>
+                            <!-- ================= the other section will start from here ======= -->
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
