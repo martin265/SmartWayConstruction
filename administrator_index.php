16188c54d7641438c2eb0dde2c48aba41149024e
martin265 <martinsilungwe12@gmail.com>
2024-02-12 14:25:05 +0200

    still in progress

--- administrator_index.php
+++ administrator_index.php
@@ -1,5 +1,35 @@
 <?php
 
+// ================== including the connection class here ============//
+include("Config/connection.php");
+$connection = new Connection("localhost", "root", "", "SmartWayConstruction");
+$connection->EstablishConnection();
+$conn = $connection->get_connection();
+
+
+// ================== function to count the databse records here ============ //
+function countPatientRecords($conn) {
+    try {
+        $sqlCommand = "SELECT COUNT(*) AS total_records FROM JobDetails";
+        // =========== running the query here ==============//
+        $results = mysqli_query($conn, $sqlCommand);
+        // ============ checking is there available results ============ //
+        if ($results) {
+            // fetching the results as an associative array ========= //
+            $row = mysqli_fetch_assoc($results);
+            $totalRecords = $row["total_records"];
+
+            return $totalRecords;
+        }
+    }catch(Exception $ex) {
+        print($ex);
+    }
+}
+
+
+$totalRecords = countPatientRecords($conn);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
